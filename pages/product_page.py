e544f7f82afe02d204de8e526dea38499a0bce57
Anton Kovalev <kovalev8992@gmail.com>
2022-08-03 17:50:39 +0300

    repair failed test, add parametrize for 10 pages

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,7 +7,15 @@ class ProductPage(BasePage):
         button_add_to_basket.click()
 
     def should_be_message_about_name_of_product(self):
-        assert self.is_element_present(*ProductPageLocators.BOOK_NAME_IN_BASKET) == self.is_element_present(*ProductPageLocators.BOOK_NAME)
+        book_name_in_basket = self.browser.find_element(*ProductPageLocators.BOOK_NAME_IN_BASKET).text
+        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+        print(book_name_in_basket)
+        print(book_name)
+        assert book_name == book_name_in_basket
 
     def should_be_book_price_equals_basket_price(self):
-        assert self.is_element_present(*ProductPageLocators.PRICE_IN_BASKET) == self.is_element_present(*ProductPageLocators.BOOK_PRICE)
+        price_in_basket = self.browser.find_element(*ProductPageLocators.PRICE_IN_BASKET).text
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
+        print(price_in_basket)
+        print(book_price)
+        assert book_price == price_in_basket
